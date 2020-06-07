a9f46b2f5d4ae686cad7b70d8e8455261c23b186
***** *** <rohroexperiment@rohroexperiment.com>
2020-06-07 14:59:26 +0530

    test blobbing

--- incrementor.py
+++ incrementor.py
@@ -1,3 +1,7 @@
+from os import urandom
+from base64 import b32encode
+
+
 class Noob(object):
     def __init__(self):
         self.val = 0
@@ -8,10 +12,12 @@ class Noob(object):
 
 class Blob(object):
     def __init__(self):
-        self.data = b''
+        self.id = b32encode(urandom(32)).decode()
 
     def set_data(self, data):
-        self.data = data
+        with open(self.id, 'wb') as file:
+            file.write(data)
 
     def get_data(self):
-        return self.data
+        with open(self.id, 'rb') as file:
+            return file.read()
